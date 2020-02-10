4c4825af0a89f865fb24626544b359231ac35d42
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2020-02-10 20:24:15 -0300

    lastest edits

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,31 +1,13 @@
 import React, { Component } from "react";
-import { List, ListItem, ListItemContent } from "react-mdl";
 
 class Display extends Component {
     constructor(props) {
         super(props);
-        // this.state = {players: []}
     }
-
-    // componentDidMount() {
-    //     axios.get('http://localhost:5000/api/players')
-    //     .then(res => {
-    //         console.log(res);
-    //         this.setState({ players: res.data })
-    //     })
-    //     .catch(err => console.log(err))
-    // }
 
     render() {
         return (
-            // <List style={{ textAlign: 'left' }}>
-            //     {this.props.participants.map(participant => (
-            //             <ListItem twoline className="player" style={{ listStyle: 'none' }}>
-            //                 <ListItemContent avatar="person" subtitle={participant.country}>{participant.name}</ListItemContent>
-            //                 {/* <ListItemContent className="country"><p>{participant.country}</p></ListItemContent> */}
-            //             </ListItem>
-            //     ))}
-            // </List>
+
             <div className="player-container">
                 {this.props.participants.map(participant => (
                     <div className="player-map">
